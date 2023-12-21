8a68f141f3406a46de2b916d758072c7f2c74b44
Poojith Kumar <109473206+Poojithkumar24@users.noreply.github.com>
2023-12-21 17:37:57 +0530

    Update sidebar.tsx

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -3,7 +3,7 @@ import { SidebarRoutes } from "./sidebar-routes"
 
 export const Sidebar = () => {
   return (
-    <div className="h-full border-r flex flex-col overflow-y-auto  shadow-sm">
+   <div className="h-full border-r flex flex-col overflow-y-auto  shadow-sm bg-gradient-to-b from-purple-500 to-pink-500">
       <div className="p-6">
         <Logo />
       </div>
