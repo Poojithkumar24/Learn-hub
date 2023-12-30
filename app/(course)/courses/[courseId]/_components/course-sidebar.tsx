b43a01195c60175ddfaf06b65a8cb88cc4650fef
Poojith Kumar <109473206+Poojithkumar24@users.noreply.github.com>
2023-12-30 20:01:38 +0530

    Update course-sidebar.tsx

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -36,7 +36,7 @@ export const CourseSidebar = async ({
   });
 
   return (
-    <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm bg-gradient-to-r from-purple-500 to-pink-500">
+    <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm bg-gradient-to-r from-purple-400 to-purple-400">
       <div className="p-8 flex flex-col border-b">
         <h1 className="font-semibold">
           {course.title}
