f292440dab8a94869ab3c0cfb64a219f9a2e20c9
poojith kumar <poojithk956@gmail.com>
2024-03-23 18:25:55 +0530

    ccc

--- app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
@@ -38,45 +37,4 @@ export async function PUT(
     console.log("[CHAPTER_ID_PROGRESS]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
-=======
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
-
-import { db } from "@/lib/db";
-
-export async function PUT(
-  req: Request,
-  { params }: { params: { courseId: string; chapterId: string } }
-) {
-  try {
-    const { userId } = auth();
-    const { isCompleted } = await req.json();
-
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    } 
-
-    const userProgress = await db.userProgress.upsert({
-      where: {
-        userId_chapterId: {
-          userId,
-          chapterId: params.chapterId,
-        }
-      },
-      update: {
-        isCompleted
-      },
-      create: {
-        userId,
-        chapterId: params.chapterId,
-        isCompleted,
-      }
-    })
-
-    return NextResponse.json(userProgress);
-  } catch (error) {
-    console.log("[CHAPTER_ID_PROGRESS]", error);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
->>>>>>> e8afb64ea0204aad8a2da98f1712818b949e5e16
 }
